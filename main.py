e5585947f3f9a349810e2118404095f13613d53b
Carlos Hernandez <95943259+CarlosHernandez1990@users.noreply.github.com>
2022-06-18 15:54:34 +0000

    encoding

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import alphabet_bank
 letter_bank = alphabet_bank.alphabet
 
 choice = input('If you would like to encode a message type "encode". If you would like to decrypt a message type "decrypt".\n').lower()
-
 message = input(f"Type in your message that you would like to {choice}:\n")
+shift = int(input(f"Type in a number for the {choice}ing of your message\n"))
 
-shift = int(input("Type in a number for the {choice}ing of your message"))
-encrypted = ""
-def encode():
+def encode(message, shift):
+  encrypted = ""
   for letter in message:
-    letter_bank[index(letter) + shift]
-    
+    current_position = letter_bank.index(letter)
+    new_position = current_position + shift
+    if new_position >= len(letter_bank):
+      new_position = len(letter_bank) - new_position
+    encrypted += letter_bank[new_position]
+    return encrypted
+
+
 
 if choice == "encode":
-  
+  encode(message, shift)
+elif 
